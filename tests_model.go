5c6e8811bc4046b978ab56187cf8c3861b503aff
leonsal <leonel.salvadori@gmail.com>
2017-03-07 21:29:06 -0300

    comments fix

--- tests_model.go
+++ tests_model.go
@@ -6,7 +6,7 @@ import (
 	"github.com/g3n/engine/math32"
 )
 
-// Sets the category and name of your test in the package global "TestMap"
+// Sets the category and name of your test in the global map "TestMap"
 // The category name choosen here starts with a "|" so it shows as the
 // last category in list. Change "model" to the name of your test.
 func init() {
@@ -14,22 +14,24 @@ func init() {
 }
 
 // This is your test object. You can store state here.
-// This name must be unique in the package
+// By convention and to avoid conflict with other demo/tests name it
+// using your test category and name.
 type testsModel struct {
-	grid *graphic.GridHelper
+	grid *graphic.GridHelper // Pointer to a GridHelper created in 'Initialize'
 }
 
-// This method will be called once when the test is selected from the list
-// You can add your objects to the scene here.
-// The ctx objects contain several global objects built by the main program.
-// ctx.Scene is the scene being rendered.
+// This method will be called once when the test is selected from the list.
+// ctx is a pointer to a Context structure built by the main program.
+// It allows access to several important object such as the scene (ctx.Scene),
+// camera (ctx.Camera) and the window (ctx.Win) among others.
+// You can build your scene adding your objects to the ctx.Scene.
 func (t *testsModel) Initialize(ctx *Context) {
 
 	// Show axis helper
 	ah := graphic.NewAxisHelper(1.0)
 	ctx.Scene.Add(ah)
 
-	// Creates grid helper and saves its pointer in the test state
+	// Creates a grid helper and saves its pointer in the test state
 	t.grid = graphic.NewGridHelper(50, 1, &math32.Color{0.4, 0.4, 0.4})
 	ctx.Scene.Add(t.grid)
 
