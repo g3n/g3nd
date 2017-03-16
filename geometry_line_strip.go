94e8f1f9b1245538d121e6fa86e65d9044586bd8
leonsal <leonel.salvadori@gmail.com>
2017-03-16 15:49:05 -0300

    modified geometry_line_strip to check for VBO item calculation error

--- geometry_line_strip.go
+++ geometry_line_strip.go
@@ -16,40 +16,70 @@ type LineStrip struct{}
 
 func (t *LineStrip) Initialize(ctx *Context) {
 
-	// Creates geometry
-	geom := geometry.NewGeometry()
+	//
+	// Creates line strip geometry using ONE interlaced buffer for vertices and colors
+	//
+	geom1 := geometry.NewGeometry()
+	buffer := math32.NewArrayF32(0, 0)
+	buffer.Append(
+		0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+		0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
+		0.0, 0.0, -0.5, 0.0, 0.0, 1.0,
+		0.0, 0.5, 0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, 0.5, 0.0, 1.0, 0.0,
+		-0.5, 0.0, 0.0, 0.0, 0.0, 1.0,
+		0.0, -0.5, 0.0, 1.0, 0.0, 0.0,
+	)
+	geom1.AddVBO(gls.NewVBO().
+		AddAttrib("VertexPosition", 3).
+		AddAttrib("VertexColor", 3).
+		SetBuffer(buffer),
+	)
+
+	// Creates basic material
+	mat1 := material.NewBasic()
+	mat1.SetLineWidth(2.0)
+
+	// Creates line strip with the specified geometry and material
+	lines1 := graphic.NewLineStrip(geom1, mat1)
+	lines1.SetPosition(-0.6, 0, 0)
+	ctx.Scene.Add(lines1)
+
+	//
+	// Creates line strip geometry using TWO buffers: one for vertices and one for colors
+	//
+	geom2 := geometry.NewGeometry()
 	vertices := math32.NewArrayF32(0, 32)
 	vertices.Append(
 		0.0, 0.0, 0.0,
 		0.5, 0.0, 0.0,
 		0.0, 0.0, -0.5,
 		0.0, 0.5, 0.0,
-		0.0, -0.5, 0.0,
-		0.0, 0.0, -0.5,
+		0.0, 0.0, 0.5,
 		-0.5, 0.0, 0.0,
-		0.0, 0.0, 0.0,
+		0.0, -0.5, 0.0,
 	)
 	colors := math32.NewArrayF32(0, 32)
 	colors.Append(
 		1.0, 0.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 0.0, 1.0,
 		1.0, 0.0, 0.0,
 		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
 		0.0, 0.0, 1.0,
-		0.0, 0.0, 1.0,
-		0.0, 1.0, 1.0,
-		0.0, 1.0, 1.0,
+		1.0, 0.0, 0.0,
 	)
-	geom.AddVBO(gls.NewVBO().AddAttrib("VertexPosition", 3).SetBuffer(vertices))
-	geom.AddVBO(gls.NewVBO().AddAttrib("VertexColor", 3).SetBuffer(colors))
+	geom2.AddVBO(gls.NewVBO().AddAttrib("VertexPosition", 3).SetBuffer(vertices))
+	geom2.AddVBO(gls.NewVBO().AddAttrib("VertexColor", 3).SetBuffer(colors))
 
 	// Creates basic material
-	mat := material.NewBasic()
-	mat.SetLineWidth(2.0)
+	mat2 := material.NewBasic()
+	mat2.SetLineWidth(2.0)
 
 	// Creates line strip with the specified geometry and material
-	lines := graphic.NewLineStrip(geom, mat)
-	ctx.Scene.Add(lines)
+	lines2 := graphic.NewLineStrip(geom2, mat2)
+	lines2.SetPosition(0.6, 0, 0)
+	ctx.Scene.Add(lines2)
 }
 
 func (t *LineStrip) Render(ctx *Context) {
