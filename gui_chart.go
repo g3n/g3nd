6ccaeeb5051c62172fde400bf047258419638a99
leonsal <leonel.salvadori@gmail.com>
2017-04-01 10:02:24 -0300

    chart demo

--- gui_chart.go
+++ gui_chart.go
@@ -20,18 +20,18 @@ func (t *GuiChart) Initialize(ctx *Context) {
 	ctx.Scene.Add(axis)
 
 	// Creates ChartLine panel
-	cl1 := gui.NewChartLine(500, 300)
-	cl1.SetMargins(10, 10, 10, 10)
-	cl1.SetBorders(10, 10, 10, 10)
-	cl1.SetBordersColor(&math32.Black)
-	cl1.SetColor(&math32.White)
-	cl1.SetPaddings(8, 8, 8, 8)
-	cl1.SetPaddingsColor(&math32.Green)
-	cl1.SetPosition(0, 0)
+	chart := gui.NewChart(500, 300)
+	chart.SetMargins(10, 10, 10, 10)
+	chart.SetBorders(2, 2, 2, 2)
+	chart.SetBordersColor(&math32.Black)
+	chart.SetColor(&math32.White)
+	chart.SetPaddings(8, 8, 8, 8)
+	//chart.SetPaddingsColor(&math32.Green)
+	chart.SetPosition(0, 0)
 
-	cl1.SetFormatX("%3.2f")
-	cl1.SetFormatY("%2.1f")
-	ctx.Gui.Add(cl1)
+	chart.SetFormatX("%3.2f")
+	chart.SetFormatY("%2.1f")
+	ctx.Gui.Add(chart)
 
 	minY := float32(-12.0)
 	maxY := float32(12.0)
@@ -40,52 +40,49 @@ func (t *GuiChart) Initialize(ctx *Context) {
 	stepX := float32(2.0)
 	countStepX := float32(2.0)
 
-	cl1.SetRangeY(minY, maxY)
+	chart.SetRangeY(minY, maxY)
 
 	// Graph1
-	var g1 *gui.LineGraph
-	//data1 := []float32{0, 1, 1.5, 3, 20, 5, -8, 7, 7.5, 9, 9.5}
-	//data1 := []float32{0, 1, -1, 2, -2, 3, -3, 4}
+	var g1 *gui.Graph
 	stepX = float32(2.0)
 	countStepX = float32(2.0)
-
 	data1 := make([]float32, 0)
 	var x float32
 	for x = 0; x < 2*math.Pi*10; x += 0.1 {
-		//data1 = append(data1, 10*math32.Sin(x)*math32.Sin(x/10))
-		data1 = append(data1, 10*math32.Sin(x))
+		data1 = append(data1, 10*math32.Sin(x)*math32.Sin(x/10))
 	}
 	stepX = float32(2.0)
 	countStepX = float32(20)
 
 	cbG1 := gui.NewCheckBox("Graph1")
-	cbG1.SetPosition(cl1.Position().X+10, cl1.Position().Y+cl1.Height()+10)
+	cbG1.SetPosition(chart.Position().X+10, chart.Position().Y+chart.Height()+10)
 	cbG1.Subscribe(gui.OnChange, func(name string, ev interface{}) {
 		if cbG1.Value() {
-			g1 = cl1.AddGraph(&math32.Color{0, 0, 1}, data1)
+			g1 = chart.AddLineGraph(&math32.Color{0, 0, 1}, data1)
 			g1.SetLineWidth(2.0)
 		} else {
-			cl1.RemoveGraph(g1)
+			chart.RemoveGraph(g1)
 			g1 = nil
 		}
 	})
 	ctx.Gui.Add(cbG1)
+	cbG1.SetValue(true)
 
 	// Graph2
-	var g2 *gui.LineGraph
+	var g2 *gui.Graph
 	data2 := make([]float32, 0)
 	var x2 float32
 	for x2 = 0; x2 < 2*math.Pi*10; x2 += 0.1 {
-		data2 = append(data2, 5*math32.Cos(x2/3))
+		data2 = append(data2, -2+5*math32.Cos(x2/3))
 	}
 	cbG2 := gui.NewCheckBox("Graph2")
 	cbG2.SetPosition(cbG1.Position().X+cbG1.Width()+10, cbG1.Position().Y)
 	cbG2.Subscribe(gui.OnChange, func(name string, ev interface{}) {
 		if cbG2.Value() {
-			g2 = cl1.AddGraph(&math32.Color{1, 0, 0}, data2)
+			g2 = chart.AddLineGraph(&math32.Color{1, 0, 0}, data2)
 			g2.SetLineWidth(2.0)
 		} else {
-			cl1.RemoveGraph(g2)
+			chart.RemoveGraph(g2)
 			g2 = nil
 		}
 	})
@@ -96,12 +93,10 @@ func (t *GuiChart) Initialize(ctx *Context) {
 	cbTitle.SetPosition(cbG1.Position().X, cbG1.Position().Y+cbG1.Height()+10)
 	cbTitle.Subscribe(gui.OnChange, func(name string, ev interface{}) {
 		if cbTitle.Value() {
-			title := gui.NewLabel("Chart Title")
-			title.SetBgColor(&math32.White)
-			title.SetFontSize(16)
-			cl1.SetTitle(title)
+			chart.SetTitle("Chart Title")
+			chart.Title().SetFontSize(16)
 		} else {
-			cl1.SetTitle(nil)
+			chart.SetTitle("")
 		}
 	})
 	cbTitle.SetValue(true)
@@ -112,10 +107,11 @@ func (t *GuiChart) Initialize(ctx *Context) {
 	cbX.SetPosition(cbTitle.Position().X+cbTitle.Width()+10, cbTitle.Position().Y)
 	cbX.Subscribe(gui.OnChange, func(name string, ev interface{}) {
 		if cbX.Value() {
-			cl1.SetScaleX(5, &math32.Color{0.8, 0.8, 0.8})
-			cl1.SetRangeX(firstX, stepX, countStepX)
+			chart.SetScaleX(5, &math32.Color{0.8, 0.8, 0.8})
+			chart.SetFontSizeX(13)
+			chart.SetRangeX(firstX, stepX, countStepX)
 		} else {
-			cl1.ClearScaleX()
+			chart.ClearScaleX()
 		}
 	})
 	cbX.SetValue(true)
@@ -126,24 +122,23 @@ func (t *GuiChart) Initialize(ctx *Context) {
 	cbY.SetPosition(cbX.Position().X+cbX.Width()+10, cbX.Position().Y)
 	cbY.Subscribe(gui.OnChange, func(name string, ev interface{}) {
 		if cbY.Value() {
-			cl1.SetScaleY(5, &math32.Color{0.8, 0.8, 0.8})
+			chart.SetScaleY(5, &math32.Color{0.8, 0.8, 0.8})
+			chart.SetFontSizeY(13)
 		} else {
-			cl1.ClearScaleY()
+			chart.ClearScaleY()
 		}
 	})
 	cbY.SetValue(true)
 	ctx.Gui.Add(cbY)
 
-	// startX
-	sG1sx := gui.NewHSlider(100, 20)
-	sG1sx.SetPosition(cbTitle.Position().X, cbTitle.Position().Y+cbTitle.Height()+10)
-	sG1sx.SetValue(0)
-	sG1sx.SetText(fmt.Sprintf("startX:%d", startX))
-	sG1sx.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
-		startX = int(sG1sx.Value() * float32(len(data1)))
-		sG1sx.SetText(fmt.Sprintf("startX:%d", startX))
-		firstX = float32(startX)
-		cl1.SetRangeX(firstX, stepX, countStepX)
+	// startX ranger
+	rStartx := newRanger(100, 20, 0, float32(len(data1)), "startX:%2.0f")
+	rStartx.SetPosition(cbTitle.Position().X, cbTitle.Position().Y+cbTitle.Height()+10)
+	rStartx.SetValue(0)
+	rStartx.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
+		firstX = float32(int(rStartx.Value()))
+		startX = int(firstX)
+		chart.SetRangeX(firstX, stepX, countStepX)
 		if g1 != nil {
 			g1.SetData(data1[startX:])
 		}
@@ -151,56 +146,101 @@ func (t *GuiChart) Initialize(ctx *Context) {
 			g2.SetData(data2[startX:])
 		}
 	})
-	ctx.Gui.Add(sG1sx)
+	ctx.Gui.Add(rStartx)
 
-	// step and countStepX
-	sG1cx := gui.NewHSlider(100, 20)
-	sG1cx.SetPosition(sG1sx.Position().X+sG1sx.Width()+10, sG1sx.Position().Y)
-	sG1cx.SetValue(float32(stepX))
-	sG1cx.SetText(fmt.Sprintf("stepX:%2.1f", stepX))
-	sG1cx.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
-		stepX = float32(math.Trunc(float64(1 + 20*sG1cx.Value())))
+	// step and countStepX ranger
+	rStepx := newRanger(100, 20, 1, 20, "stepX:%2.0f")
+	rStepx.SetPosition(rStartx.Position().X+rStartx.Width()+10, rStartx.Position().Y)
+	rStepx.SetValue(float32(stepX))
+	rStepx.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
+		stepX = float32(int(rStepx.Value()))
 		countStepX = 10 * stepX
-		sG1cx.SetText(fmt.Sprintf("stepX:%2.1f", stepX))
-		cl1.SetRangeX(firstX, stepX, countStepX)
+		chart.SetRangeX(firstX, stepX, countStepX)
 	})
-	ctx.Gui.Add(sG1cx)
+	ctx.Gui.Add(rStepx)
 
-	// minY
-	sG1y0 := gui.NewHSlider(100, 20)
-	sG1y0.SetPosition(sG1cx.Position().X+sG1cx.Width()+10, sG1cx.Position().Y)
-	sG1y0.SetText(fmt.Sprintf("minY:%3.1f", minY))
-	sG1y0.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
-		minY = -20 + 20*sG1y0.Value()
-		sG1y0.SetText(fmt.Sprintf("minY:%3.1f", minY))
-		cl1.SetRangeY(minY, maxY)
+	// minY ranger
+	rMiny := newRanger(100, 20, -20, 20, "minY:%3.1f")
+	rMiny.SetPosition(rStepx.Position().X+rStepx.Width()+10, rStepx.Position().Y)
+	rMiny.SetValue(minY)
+	rMiny.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
+		minY = rMiny.Value()
+		chart.SetRangeY(minY, maxY)
 	})
-	ctx.Gui.Add(sG1y0)
+	ctx.Gui.Add(rMiny)
 
-	// maxY
-	sG1y1 := gui.NewHSlider(100, 20)
-	sG1y1.SetPosition(sG1y0.Position().X+sG1y0.Width()+10, sG1y0.Position().Y)
-	sG1y1.SetText(fmt.Sprintf("maxY:%3.1f", maxY))
-	sG1y1.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
-		maxY = 20 * sG1y1.Value()
-		sG1y1.SetText(fmt.Sprintf("maxY:%3.1f", maxY))
-		cl1.SetRangeY(minY, maxY)
+	// maxY ranger
+	rMaxy := newRanger(100, 20, -20, 20, "maxY:%3.1f")
+	rMaxy.SetPosition(rMiny.Position().X+rMiny.Width()+10, rMiny.Position().Y)
+	rMaxy.SetValue(maxY)
+	rMaxy.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
+		maxY = rMaxy.Value()
+		chart.SetRangeY(minY, maxY)
 	})
-	ctx.Gui.Add(sG1y1)
+	ctx.Gui.Add(rMaxy)
 
-	// Y auto range
-	cbYa := gui.NewCheckBox("Y auto")
-	cbYa.SetPosition(sG1y1.Position().X+sG1y1.Width()+10, sG1y1.Position().Y)
-	cbYa.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
-		if cbYa.Value() {
-			cl1.SetRangeYauto(true)
+	// Y auto range checkbox
+	cbAutoy := gui.NewCheckBox("Y auto")
+	cbAutoy.SetPosition(rMaxy.Position().X+rMaxy.Width()+10, rMaxy.Position().Y)
+	cbAutoy.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
+		if cbAutoy.Value() {
+			chart.SetRangeYauto(true)
+			minY, maxY = chart.RangeY()
+			rMiny.SetEnabled(false)
+			rMaxy.SetEnabled(false)
+			rMiny.SetValue(minY)
+			rMaxy.SetValue(maxY)
 		} else {
-			cl1.SetRangeYauto(false)
+			chart.SetRangeYauto(false)
+			rMiny.SetEnabled(true)
+			rMaxy.SetEnabled(true)
+
 		}
 	})
-	ctx.Gui.Add(cbYa)
+	ctx.Gui.Add(cbAutoy)
 }
 
 func (t *GuiChart) Render(ctx *Context) {
 
+}
+
+type ranger struct {
+	*gui.Slider
+	min    float32
+	max    float32
+	val    float32
+	format string
+}
+
+func newRanger(width, height, min, max float32, format string) *ranger {
+
+	r := new(ranger)
+	r.Slider = gui.NewHSlider(width, height)
+	r.min = min
+	r.max = max
+	r.format = format
+
+	r.Slider.Subscribe(gui.OnChange, func(evname string, ev interface{}) {
+		r.SetText(fmt.Sprintf(r.format, r.Value()))
+	})
+	return r
+}
+
+func (r *ranger) Value() float32 {
+
+	return r.min + (r.max-r.min)*r.Slider.Value()
+}
+
+func (r *ranger) SetValue(val float32) {
+
+	if val < r.min {
+		r.val = r.min
+	} else if val > r.max {
+		r.val = r.max
+	} else {
+		r.val = val
+	}
+	sv := (r.val - r.min) / (r.max - r.min)
+	r.SetText(fmt.Sprintf(r.format, r.val))
+	r.Slider.SetValue(sv)
 }
