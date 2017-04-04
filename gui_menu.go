0219fd394351667c343d27a9a964934302fa262d
leonsal <leonel.salvadori@gmail.com>
2017-04-04 17:28:09 -0300

    gui menu dev

--- gui_menu.go
+++ gui_menu.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/g3n/engine/gui"
+)
+
 func init() {
 	TestMap["gui.menu"] = &GuiMenu{}
 }
@@ -8,6 +12,11 @@ type GuiMenu struct {
 }
 
 func (t *GuiMenu) Initialize(ctx *Context) {
+
+	m1 := gui.NewMenu()
+	m1.SetPosition(10, 10)
+
+	ctx.Gui.Add(m1)
 }
 
 func (t *GuiMenu) Render(ctx *Context) {
