70ce28ba8d8b2f4f23e46fd0b987e72e4d56cd4a
leonsal <leonel.salvadori@gmail.com>
2017-04-07 19:22:46 -0300

    gui_menu dev

--- gui_menu.go
+++ gui_menu.go
@@ -16,6 +16,12 @@ func (t *GuiMenu) Initialize(ctx *Context) {
 	m1 := gui.NewMenu()
 	m1.SetPosition(10, 10)
 
+	m1.AddItem("Menu1 Option1")
+	m1.AddItem("Menu1 Option2 (longer)")
+	m1.AddItem("Menu1 Option3")
+	m1.AddSeparator()
+	m1.AddItem("Menu1 Option4")
+
 	ctx.Gui.Add(m1)
 }
 
