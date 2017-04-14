64ce19f2b90dafc45b9ba4ad6628f4122b2010e7
leonsal <leonel.salvadori@gmail.com>
2017-04-14 14:11:59 -0300

    gui menu dev

--- gui_menu.go
+++ gui_menu.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/g3n/engine/gui"
 	"github.com/g3n/engine/gui/assets"
+	"github.com/g3n/engine/window"
 )
 
 func init() {
@@ -17,26 +18,39 @@ func (t *GuiMenu) Initialize(ctx *Context) {
 	m1 := gui.NewMenu()
 	m1.SetPosition(10, 10)
 
-	m1.AddItem("Menu1 Option1")
-	m1.AddItem("Menu1 Option2 (longer)")
-	m1.AddItem("Menu1 Option3")
+	m1.AddOption("Menu1 Option1")
+	m1.AddOption("Menu1 Option2 (longer)")
+	m1i1 := m1.AddOption("Menu1 Option3")
+	m1i1.SetEnabled(false)
 	m1.AddSeparator()
-	m1.AddItem("Menu1 Option4")
+	m1.AddOption("Menu1 Option4")
 	ctx.Gui.Add(m1)
 
 	m2 := gui.NewMenu()
 	m2.SetPosition(m1.Position().X+m1.Width()+10, 10)
-
-	m2i1 := m2.AddItem("Menu2 Option1")
+	m2i1 := m2.AddOption("Menu2 Option1")
 	m2i1.SetIcon(assets.Add)
-
-	m2i2 := m2.AddItem("Menu2 Option2 (longer)")
+	m2i2 := m2.AddOption("Menu2 Option2 (longer)")
 	m2i2.SetIcon(assets.ArrowBack)
-
-	m2.AddItem("Menu2 Option3")
+	m2i2.SetShortcut(window.ModShift, window.KeyA)
+	m2.AddOption("Menu2 Option3")
 	m2.AddSeparator()
-	m2.AddItem("Menu2 Option4")
+	m2i4 := m2.AddOption("Menu2 Option4")
+	m2i4.SetShortcut(window.ModControl, window.KeyB)
 	ctx.Gui.Add(m2)
+
+	m3 := gui.NewMenu()
+	m3.SetPosition(m1.Position().X, m1.Position().Y+m1.Height()+10)
+	m3.AddOption("Menu3 Option1")
+	m3.AddOption("Menu3 Option2")
+	m3sm1 := gui.NewMenu()
+	m3sm1.AddOption("Menu3/Sub Menu1 Option1")
+	m3sm1.AddOption("Menu3/Sub Menu1 Option2")
+	m3.AddSeparator()
+	m3.AddMenu("Menu3 Sub Menu1", m3sm1)
+	m3.AddOption("Menu3 Option3")
+
+	ctx.Gui.Add(m3)
 }
 
 func (t *GuiMenu) Render(ctx *Context) {
