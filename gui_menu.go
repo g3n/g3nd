857e940559f9214c6ce3a32b8f1cf489e52fbddd
leonsal <leonel.salvadori@gmail.com>
2017-04-14 15:51:12 -0300

    gui menu dev

--- gui_menu.go
+++ gui_menu.go
@@ -43,12 +43,19 @@ func (t *GuiMenu) Initialize(ctx *Context) {
 	m3.SetPosition(m1.Position().X, m1.Position().Y+m1.Height()+10)
 	m3.AddOption("Menu3 Option1")
 	m3.AddOption("Menu3 Option2")
+
 	m3sm1 := gui.NewMenu()
-	m3sm1.AddOption("Menu3/Sub Menu1 Option1")
-	m3sm1.AddOption("Menu3/Sub Menu1 Option2")
+	m3sm1.AddOption("Menu3/SubMenu1/Option1")
+	m3sm1.AddOption("Menu3/SubMenu1/Option2")
+	m3sm1.AddSeparator()
+	m3sm1sm2 := gui.NewMenu()
+	m3sm1sm2.AddOption("Menu3/SubMenu1/SubMenu2/Option1")
+	m3sm1sm2.AddOption("Menu3/SubMenu1/SubMenu2/Option2")
+	m3sm1.AddMenu("Menu3/SubMenu1/SubMenu2", m3sm1sm2)
+
 	m3.AddSeparator()
-	m3.AddMenu("Menu3 Sub Menu1", m3sm1)
-	m3.AddOption("Menu3 Option3")
+	m3.AddMenu("Menu3/SubMenu1", m3sm1)
+	m3.AddOption("Menu3/Option3")
 
 	ctx.Gui.Add(m3)
 }
