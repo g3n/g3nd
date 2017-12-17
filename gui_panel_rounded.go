301b3411a835ae0dbee48b272ffa96718cd320d5
leonsal <leonel.salvadori@gmail.com>
2017-12-17 10:37:20 -0200

    gui rounded panel demo

--- gui_panel_rounded.go
+++ gui_panel_rounded.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"math"
+
 	"github.com/g3n/engine/gui"
 	"github.com/g3n/engine/math32"
-	"github.com/g3n/engine/window"
 )
 
 type GuiPanelRounded struct {
@@ -13,6 +14,8 @@ type GuiPanelRounded struct {
 	p4 *gui.Panel
 	p5 *gui.Panel
 	p6 *gui.Panel
+	p7 *gui.Panel
+	p8 *gui.Panel
 }
 
 func init() {
@@ -30,17 +33,19 @@ func (t *GuiPanelRounded) Initialize(ctx *Context) {
 	t.p1.SetBordersColor(math32.NewColor("black"))
 	t.p1.SetColor(math32.NewColor("yellow"))
 	t.p1.SetPaddingsColor(math32.NewColor("green"))
+	t.p1.SetVisible(true)
 	ctx.Gui.Add(t.p1)
 
 	// Panel 2
 	t.p2 = gui.NewPanel(200, 100)
 	t.p2.SetPosition(t.p1.Position().X+t.p1.Width()+10, t.p1.Position().Y)
 	t.p2.SetMargins(2, 2, 2, 2)
-	t.p2.SetBorders(4, 4, 4, 4)
+	t.p2.SetBorders(4, 2, 4, 4)
 	t.p2.SetBordersColor(math32.NewColor("black"))
-	t.p2.SetPaddings(2, 2, 2, 2)
+	t.p2.SetPaddings(4, 2, 4, 2)
 	t.p2.SetColor(math32.NewColor("green"))
 	t.p2.SetPaddingsColor(math32.NewColor("white"))
+	t.p2.SetVisible(true)
 	ctx.Gui.Add(t.p2)
 
 	// Panel 3
@@ -53,6 +58,7 @@ func (t *GuiPanelRounded) Initialize(ctx *Context) {
 	t.p3.SetPaddings(2, 2, 2, 2)
 	t.p3.SetColor(math32.NewColor("yellow"))
 	t.p3.SetPaddingsColor(math32.NewColor("blue"))
+	t.p3.SetVisible(true)
 	ctx.Gui.Add(t.p3)
 
 	// Panel 4
@@ -63,6 +69,7 @@ func (t *GuiPanelRounded) Initialize(ctx *Context) {
 	t.p4.SetPaddings(2, 2, 2, 2)
 	t.p4.SetColor(math32.NewColor("white"))
 	t.p4.SetPaddingsColor(math32.NewColor("green"))
+	t.p4.SetVisible(true)
 	ctx.Gui.Add(t.p4)
 
 	// Panel 5
@@ -73,9 +80,10 @@ func (t *GuiPanelRounded) Initialize(ctx *Context) {
 	t.p5.SetPaddings(2, 2, 2, 2)
 	t.p5.SetColor(math32.NewColor("green"))
 	t.p5.SetPaddingsColor(math32.NewColor("yellow"))
+	t.p5.SetVisible(true)
 	ctx.Gui.Add(t.p5)
 
-	// Panel 5
+	// Panel 6
 	t.p6 = gui.NewPanel(100, 200)
 	t.p6.SetPosition(t.p5.Position().X+t.p5.Width()+10, t.p5.Position().Y)
 	t.p6.SetBorders(2, 2, 2, 2)
@@ -83,38 +91,38 @@ func (t *GuiPanelRounded) Initialize(ctx *Context) {
 	t.p6.SetPaddings(2, 2, 2, 2)
 	t.p6.SetColor(math32.NewColor("purple"))
 	t.p6.SetPaddingsColor(math32.NewColor("yellow"))
+	t.p6.SetVisible(true)
 	ctx.Gui.Add(t.p6)
-}
 
-func (t *GuiPanelRounded) onKey(evname string, ev interface{}) {
+	// Panel 7
+	t.p7 = gui.NewPanel(300, 32)
+	t.p7.SetPosition(t.p1.Position().X, t.p6.Position().Y+t.p5.Height()+10)
+	t.p7.SetBorders(1, 1, 1, 1)
+	t.p7.SetBordersColor(math32.NewColor("black"))
+	t.p7.SetColor(math32.NewColor("white"))
+	t.p7.SetVisible(true)
+	ctx.Gui.Add(t.p7)
 
-	kev := ev.(*window.KeyEvent)
-	if kev.Action == window.Release {
-		return
-	}
-	switch kev.Keycode {
-	case window.KeyB:
-		for _, ichild := range t.p3.Children() {
-			pan := ichild.(*gui.Panel)
-			pan.SetBounded(!pan.Bounded())
-		}
-	}
-
+	// Panel 8
+	t.p8 = gui.NewPanel(32, 300)
+	t.p8.SetPosition(t.p7.Position().X+t.p7.Width()+10, t.p7.Position().Y)
+	t.p8.SetBorders(1, 1, 1, 1)
+	t.p8.SetBordersColor(math32.NewColor("black"))
+	t.p8.SetColor(math32.NewColor("yellow"))
+	t.p8.SetVisible(true)
+	ctx.Gui.Add(t.p8)
 }
 
 func (t *GuiPanelRounded) Render(ctx *Context) {
 
-	//	time := ctx.Win.GetTime()
-	//	delta := float32((math.Sin(time) + 1.0) / 2)
-	//	t.p1.SetRoundness(delta, delta, delta, delta)
-	//	t.p2.SetRoundness(delta, delta, delta, delta)
-	//	t.p3.SetRoundness(delta, delta, delta, delta)
-	//	t.p4.SetRoundness(delta, delta, 0, 0)
-	//	t.p5.SetRoundness(0, delta, delta, 0)
-	//	t.p6.SetRoundness(0, delta, 0, delta*2)
-	//	//	maxWidth := 200
-	//	//	maxHeight := 100
-	//	//	t.p1.SetSize(float32(delta)*float32(maxWidth), float32(delta)*float32(maxHeight))
-	//	//	t.p2.SetContentSize(float32(delta)*float32(maxWidth), float32(delta)*float32(maxHeight))
-	//	//	t.p3.SetPosition(50+60*math32.Sin(float32(time)), t.p3.Position().Y)
+	time := ctx.Win.GetTime()
+	delta := float32((math.Sin(time) + 1.0) / 2)
+	t.p1.SetRoundness(delta, delta, delta, delta)
+	t.p2.SetRoundness(delta, delta, delta, delta)
+	t.p3.SetRoundness(delta, delta, delta, delta)
+	t.p4.SetRoundness(delta, delta, 0, 0)
+	t.p5.SetRoundness(0, delta, delta, 0)
+	t.p6.SetRoundness(0, delta, 0, delta*2)
+	t.p7.SetRoundness(delta, delta, delta, delta)
+	t.p8.SetRoundness(delta, delta, delta, delta)
 }
