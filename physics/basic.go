613636386599390f7b872f9d0a00bceca581c8f6
danaugrs <danaugrs@gmail.com>
2018-05-18 14:09:32 -0700

    physics dev

--- physics/basic.go
+++ physics/basic.go
@@ -1,10 +1,16 @@
-package physicssss
+package physics
 
 import (
 	"github.com/g3n/engine/graphic"
 	"github.com/g3n/g3nd/app"
 	"github.com/g3n/g3nd/demos"
 	"github.com/g3n/engine/window"
+	"github.com/g3n/engine/physics"
+	"github.com/g3n/engine/math32"
+	"github.com/g3n/engine/geometry"
+	"math"
+	"github.com/g3n/engine/material"
+	"github.com/g3n/engine/light"
 )
 
 func init() {
@@ -12,17 +18,38 @@ func init() {
 }
 
 type PhysicsBasic struct {
-	// TODO
+	sim *physics.Simulation
+	rb  *physics.Particle
 }
 
 func (t *PhysicsBasic) Initialize(a *app.App) {
 
 	axis := graphic.NewAxisHelper(1)
 	a.Scene().Add(axis)
+
+	pl := light.NewPoint(math32.NewColor("white"), 1.0)
+	pl.SetPosition(1, 0, 0)
+	a.Scene().Add(pl)
+
+	t.sim = physics.NewSimulation()
+	gravity := physics.NewConstantForceField(&math32.Vector3{0,-9.8,0})
+	//gravity := physics.NewAttractorForceField(&math32.Vector3{0.1,1,0}, 1)
+	t.sim.AddForceField(gravity)
+
+	// Creates sphere 1
+	sphereGeom := geometry.NewSphere(0.1, 16, 16, 0, math.Pi*2, 0, math.Pi)
+	mat := material.NewPhong(&math32.Color{1, 1, 1})
+	sphere1 := graphic.NewMesh(sphereGeom, mat)
+	a.Scene().Add(sphere1)
+
+	t.rb = physics.NewParticle(sphere1)
+	t.sim.AddParticle(t.rb)
+
 }
 
 func (t *PhysicsBasic) Render(a *app.App) {
 
+	t.sim.Step(a.FrameDelta())
 }
 
 func (t *PhysicsBasic) onKey(a *app.App, ev interface{}) {
